063437818d57594724998a2b5ae74010afe39002
Erin Yanacek <erinyanacek@gmail.com>
2025-08-06 12:48:55 -0400

    Fix Sanity configuration and add schemas for deployment

---
+++ schemaTypes/index.js
@@ -0,0 +1,21 @@
+import property from '../backend_schemas/property'
+import propertyImage from '../backend_schemas/propertyImage'
+import review from '../backend_schemas/review'
+import traveller from '../backend_schemas/traveller'
+import host from '../backend_schemas/host'
+import person from '../backend_schemas/person'
+import pettingZoo from '../backend_schemas/pettingZoo'
+import animal from '../backend_schemas/animal'
+import amenity from '../backend_schemas/amenity'
+
+export const schemaTypes = [
+  property,
+  propertyImage,
+  review,
+  traveller,
+  host,
+  person,
+  pettingZoo,
+  animal,
+  amenity,
+]
